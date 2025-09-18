5391e19e3f4bf06862890b71124f2a43ebbefadd
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-09-18 13:50:53 +0000

    refactor: updated to LazyVim 15

--- nvim/lua/plugins/mason-workaround.lua
+++ nvim/lua/plugins/mason-workaround.lua
@@ -1,4 +1,8 @@
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  "mason-org/mason-lspconfig.nvim",
+  opts = {},
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
 }
