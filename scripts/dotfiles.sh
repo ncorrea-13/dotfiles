54c131f73f8763d69ce95976aa7e6ccb10d3d3b0
Nicolás Correa <correanicolas1910@gmail.com>
2025-11-05 10:26:37 -0300

    feature: added script folder

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,14 @@
+cp picom.conf ~/Projects/things/dotfiles/
+cp dotfiles.sh ~/Projects/things/dotfiles/scripts/
+cp -r i3 ~/Projects/things/dotfiles/
+cp -r i3blocks ~/Projects/things/dotfiles/
+cp -r dunst ~/Projects/things/dotfiles/
+cp -r btop ~/Projects/things/dotfiles/
+cp -r fastfetch ~/Projects/things/dotfiles/
+cp -r wezterm ~/Projects/things/dotfiles/
+cp -r zellij ~/Projects/things/dotfiles/
+cp -r cava ~/Projects/things/dotfiles/
+cp -r nvim ~/Projects/things/dotfiles/
+cp -r rofi ~/Projects/things/dotfiles/
+cp -r vifm ~/Projects/things/dotfiles/
+cp -r posting ~/Projects/things/dotfiles/
