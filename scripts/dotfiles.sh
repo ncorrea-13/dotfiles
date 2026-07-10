f75d687c28c063e0529067f49aa6c60ed6051017
Nicolás Correa <89622611+ncorrea-13@users.noreply.github.com>
2026-07-10 00:45:15 -0300

    feat: archive dir

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -11,7 +11,5 @@ cp -r wezterm ~/Projects/things/dotfiles/.config/
 cp -r zellij ~/Projects/things/dotfiles/.config/
 cp -r cava ~/Projects/things/dotfiles/.config/
 cp -r nvim ~/Projects/things/dotfiles/.config/
-cp -r rofi ~/Projects/things/dotfiles/.config/
-cp -r vifm ~/Projects/things/dotfiles/.config/
 cp -r posting ~/Projects/things/dotfiles/.config/
 cp -r mpv ~/Projects/things/dotfiles/.config/
