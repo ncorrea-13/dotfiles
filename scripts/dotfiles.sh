db6210a9106b3466a8150c8fd17294cb16e84ceb
Nicolás Correa <89622611+ncorrea-13@users.noreply.github.com>
2026-07-12 11:55:00 -0300

    refactor: Update doftiles.sh

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-cp dotfiles.sh ~/Projects/things/dotfiles/scripts/
-cp topgrade.toml ~/Projects/things/dotfiles/.config/
-cp -r sway ~/Projects/things/dotfiles/.config/
-cp -r waybar ~/Projects/things/dotfiles/.config/
-cp -r dunst ~/Projects/things/dotfiles/.config/
-cp -r btop ~/Projects/things/dotfiles/.config/
-cp -r fastfetch ~/Projects/things/dotfiles/.config/
-cp -r wezterm ~/Projects/things/dotfiles/.config/
-cp -r zellij ~/Projects/things/dotfiles/.config/
-cp -r cava ~/Projects/things/dotfiles/.config/
-cp -r nvim ~/Projects/things/dotfiles/.config/
-cp -r posting ~/Projects/things/dotfiles/.config/
-cp -r mpv ~/Projects/things/dotfiles/.config/
+cp ~/.zshrc ~/Projects/things/dotfiles/
+cp ~/.alias ~/Projects/things/dotfiles/
+cp ~/.eget.toml ~/Projects/things/dotfiles/
+cp ~/.config/topgrade.toml ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/sway ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/waybar ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/dunst ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/btop ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/fastfetch ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/wezterm ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/zellij ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/cava ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/nvim ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/posting ~/Projects/things/dotfiles/.config/
+cp -r ~/.config/mpv ~/Projects/things/dotfiles/.config/
